1f96113a3a3994467382f7d99f9f5e87eb18ceb4
oaslan <oaslan@partner.innova.com.tr>
2022-01-16 17:04:28 +0300

    Proje oluşturulması

--- README.md
+++
@@ -1,2 +0,0 @@
-# RabbitMQ-Intro
-SignalR, RabbitMQ , API , .Net
---
+++ RabbitMQExcel_Pdf/RabbitMQ.Excel.Create/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabbitMQ.Excel.Create.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
