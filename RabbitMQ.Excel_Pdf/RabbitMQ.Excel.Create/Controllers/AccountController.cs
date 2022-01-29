3242f27598d7d29ea021bda1f961654f88827d16
oaslan <oaslan@partner.innova.com.tr>
2022-01-29 21:30:26 +0300

    login and product created.

---
+++ RabbitMQ.Excel_Pdf/RabbitMQ.Excel.Create/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Excel.Create.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Excel.Create.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager,UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email,string password)
+        {
+            var userEmail =await _userManager.FindByEmailAsync(email);
+
+            if (userEmail!=null)
+            {
+                var pswUser = _signInManager.PasswordSignInAsync(userEmail, password, true, false);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ RabbitMQ.Excel_Pdf/RabbitMQ.Excel.Create/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Excel.Create.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Excel.Create.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public ProductController(AppDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            string fileName = Guid.NewGuid().ToString();
+
+            UserFile userFile = new()
+            {
+                UserId = user.Id,
+                FileStatus = FileStatus.Creating,
+                FileName = fileName
+
+            };
+
+            await _context.UserFiles.AddAsync(userFile);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Files");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Files()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(_context.UserFiles.Where(a=>a.UserId==user.Id).ToListAsync());
+        }
+    }
+}
